2cc3abd824a577facec2dce6c6f9e4a831d1dd45
Arjun <arjunsivadas2000@gmail.com>
2024-07-31 14:54:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".swiper-wrapper", {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+});
